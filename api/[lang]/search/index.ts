d875fbdcf789493f083426bc81e19d244cb179c6
Manda Putra <mandaputra8@gmail.com>
2021-07-31 15:07:13 +0700

    feat: refactor

--- api/[lang]/search/index.ts
+++ api/[lang]/search/index.ts
@@ -1,6 +1,7 @@
 import { NowResponse, NowRequest } from '@vercel/node'
 import cheerio from 'cheerio'
-import { fetchHTML } from '../../utils/fetch'
+import { fetchHTML } from '../../../utils/fetch'
+import { getLang } from '../../../utils/lang'
 
 interface ServingList {
   name: string;
@@ -15,7 +16,6 @@ interface FoundList {
   fat: number;
   carbo: number;
   protein: number;
-  apiURL: string;
 }
 
 interface DataResponse {
@@ -31,14 +31,21 @@ export default async (
   request: NowRequest,
   response: NowResponse
 ): Promise<void> => {
-  const query: any = request.query.q
-  const page: any = request.query.pg || 0
+  const query: any = request.query.query
+  const page: any = +request.query.page || 0
+  const langConfig = getLang(String(request.query.lang))
 
-  if (!query) {
-    response.json('Please insert a query, q=??')
+  if (!langConfig) {
+    response.json({
+      error: `${request.query.lang} are not supported, for now`
+    })
   }
 
-  const html = await fetchHTML('kalori-gizi/search', {
+  if (!query) {
+    response.json({ error: 'Please insert a query, q=??' })
+  }
+
+  const html = await fetchHTML(langConfig.searchUrl, {
     q: query,
     pg: page
   })
@@ -48,13 +55,12 @@ export default async (
   $('table.generic.searchResult td.borderBottom').each((_, elem: any) => {
     const element = $(elem)
     const title = element.find('a.prominent').text()
-    const slug = element.find('a.prominent').attr('href').split('/')[3]
     const normalizeText = element
       .find('div.smallText.greyText.greyLink')
       .text()
       .replace(/(\r\n|\n|\r\t|\t|\r)/gm, '')
 
-    const splitSection = normalizeText.split('Ukuran Lainnya:')
+    const splitSection = normalizeText.split(langConfig.otherSizes)
     const splitGeneralInfoString = splitSection[0].split('-')
     const generalInfo = splitGeneralInfoString[1].split('|')
     const calories = +generalInfo[0].replace(/Kalori:|kkal/g, '') || 0
@@ -80,7 +86,6 @@ export default async (
 
     items.push({
       title,
-      apiURL: `/detail/${slug}`,
       protein,
       fat,
       carbo,
--- utils/browser.ts
+++
@@ -1,2 +0,0 @@
-// TODO: I don't know for sure, maybe later case
-
